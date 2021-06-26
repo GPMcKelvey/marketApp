73b6510253991f4b0c4f990aa92aa505607d5dc7
GPMcKelvey <gpmckelvey@gmail.com>
2021-06-26 10:30:02 -0400

    react app creation

---
+++ src/componenets/forums/forums.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const forums = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/componenets/markets/markets.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const markets = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
