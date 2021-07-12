bb5224d7a685a6c6ad1d7455f12f54f93bacd838
GPMcKelvey <gpmckelvey@gmail.com>
2021-07-12 14:31:15 -0400

    auth, login, signup, and installed material ui(will need to run an npm install after pulling down these changes

--- src/componenets/auth/Auth.jsx
+++ src/componenets/auth/Auth.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { Component } from 'react';
+import {IUser} from '../Interfaces';
 
-export const Auth = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import { Button, withStyles, Theme, Box, AppBar } from '@material-ui/core';
+import { purple } from '@material-ui/core/colors';
+
+import Login from './Login';
+import Signup from './Signup';
+import dailyPlanner from '../assets/dailyPlanner.png';
+
+const ColorButton = withStyles((theme: Theme) => ({
+    root: {
+      color: theme.palette.getContrastText(purple[300]),
+      backgroundColor: purple[300],
+      '&:hover': {
+        backgroundColor: purple[400],
+      },
+    },
+  }))(Button);
+
+
+export default class Auth extends Component{
+    constructor (props) {
+        super(props)
+        this.state = {
+            loginModal: false,
+            signupModal: false
+        }
+    };
+
+    signupModalHandler = () => {
+        this.setState({
+            signupModal: true
+        })
+    }
+
+    loginModalHandler = () => {
+        this.setState({
+            loginModal: true
+        })
+    }
+
+    exitHandler = () => {
+        this.setState({
+            loginModal: false,
+            signupModal: false
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <AppBar>
+                <Box display='flex' justifyContent='center' className='appBar'>
+                <Box pt={2} pr={40} flexGrow= '1'>
+                    <img src={dailyPlanner} style={{maxHeight: '75%'}}></img>
+                        {/* <Typography>Daily Planner</Typography> */}
+                        </Box>
+                        <Box display='flex' flexGrow='7' alignSelf= 'center' justifySelf='center'>
+                    <Box m={2} p={1} >
+                    <ColorButton variant="contained" color='primary' onClick={this.signupModalHandler}>Sign Up</ColorButton></Box>
+                    <Box m={2} p={1}>
+                    <ColorButton variant="contained" onClick={this.loginModalHandler}>Login</ColorButton></Box>
+                    </Box>
+                </Box>
+                </AppBar>
+                {this.state.loginModal ? <Login updateToken={this.props.updateToken} exitHandler={this.exitHandler} loginModal={this.state.loginModal} /> : null}
+                {this.state.signupModal ? <Signup updateToken={this.props.updateToken} exitHandler={this.exitHandler} signupModal={this.state.signupModal} /> : null}
+            </div>
+        )
+    }
+} 
--- src/componenets/auth/Login.jsx
+++ src/componenets/auth/Login.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, {Component} from 'react';
+import './AuthStyle.css';
+import APIURL from '../../helpers/environment';
+import {IUser} from '../Interfaces';
 
-export const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+import {Dialog, Button, withStyles, Theme, Box} from '@material-ui/core';
+import {purple} from '@material-ui/core/colors';
+
+const ColorButton = withStyles((theme: Theme) => ({
+    root: {
+      color: theme.palette.getContrastText(purple[300]),
+      backgroundColor: purple[300],
+      '&:hover': {
+        backgroundColor: purple[400],
+      },
+    },
+  }))(Button);
+
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: '',
+            modal: props.loginModal
+        }
+    }
+
+    
+    handleSubmit = (event) => {
+        event.preventDefault();
+        fetch(`${APIURL}/users/login`, {
+             method: 'POST',
+             body: JSON.stringify({
+                 user:{
+                     username: this.state.username, 
+                     password: this.state.password 
+                    }}),
+             headers: new Headers({
+                  'Content-Type': 'application/json'
+             })
+         })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            this.props.updateToken(data);
+        })
+        this.props.exitHandler();
+    }
+
+    inputHandler = (e) => {
+        const value = e.target.value;
+        this.setState({
+                ...this.state,
+                [e.target.name]: value,
+            })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Dialog open={this.state.modal} className='login'>  
+                    <div id='loginDiv'>
+                        <h1 className='header'>Login</h1>
+                            <form className='form-inputs' onSubmit={this.handleSubmit}>    
+                                <label> Username: </label>
+                                <Box>
+                                    <input id='username' name= 'username' type= 'text' onChange={this.inputHandler} value={this.state.username} >   
+                                    </input></Box>
+                                <br/>
+                                <label> Password:</label>
+                                <Box>
+                                    <input id='password' name='password' type='password' onChange={this.inputHandler} value={this.state.password}>
+                                    </input></Box>
+                                <br />
+                                <Box display='flex'>
+                                <Box m={1} p={1}>
+                                <ColorButton className='form-input-btn' onClick={this.handleSubmit}>Submit</ColorButton>
+                                </Box>
+                                <Box m={1} p={1}>
+                                <ColorButton className= 'form-input-btn' onClick={this.props.exitHandler}>Exit</ColorButton>
+                                </Box>
+                                </Box>
+                            </form>
+                    </div>
+                </Dialog>
+            </div>
+        )
+    }
 }
