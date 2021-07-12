7efeac71556973a304e052a8588970eaf726ef47
GPMcKelvey <gpmckelvey@gmail.com>
2021-07-12 16:50:12 -0400

    changed some naming conventions

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Cards from './components/Cards'
 
 
 // import About from './components/about/about'
-import Farmers from './components/farmers/farmers'
-import Forums from './components/forums/forums'
+import MainFarmers from './components/farmers/MainFarmers'
+import MainForums from './components/forums/MainForums'
 import Resources from './components/resources/resources'
 import Markets from './components/markets/markets'
 import Foods from './components/foods/foods'
@@ -22,8 +22,8 @@ function App() {
         <Navbar /> 
         <Switch> 
           <Route path="/" exact component={Cards} /> 
-          <Route path="/farmers" exact component={Farmers} /> 
-          <Route path="/forums" exact component={Forums} /> 
+          <Route path="/farmers" exact component={MainFarmers} /> 
+          <Route path="/forums" exact component={MainForums} /> 
           <Route path="/markets" exact component={Markets} /> 
           <Route path="/resources" exact component={Resources} /> 
           <Route path="/foods" exact component={Foods} /> 
